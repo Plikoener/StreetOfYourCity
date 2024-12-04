67b457039f874c69df6784e9370aced4b8848a32
Julian Lechner <fraujulian@lechner.top>
2024-12-04 20:43:16 +0100

    Created Project

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace StreetOfYourCity;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace StreetOfYourCity;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
