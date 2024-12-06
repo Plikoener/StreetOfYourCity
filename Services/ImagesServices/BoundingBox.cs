a415d9c7d669bd43362004d5f960b346496143cb
Plikoener <dev@plikoener.de>
2024-12-06 23:40:17 +0100

    add Helper Classes for Imageservice

---
+++ Services/ImagesServices/BoundingBox.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StreetOfYourCity.Services.LocationDataServices.Dto;
+
+namespace StreetOfYourCity.Services.ImagesServices
+{
+    public class BoundingBox
+    {
+        public MapPoint MinPoint { get; set; }
+        public MapPoint MaxPoint { get; set; }
+    }
+}
